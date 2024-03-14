64799e04f49d411a51633b0f1b0db06bb3674185
Fabrizio Bautista <130857074+fabrixbau@users.noreply.github.com>
2024-03-14 11:44:22 -0600

    se agrego /admin/ y se unio con tailwind igual se crearon sus tablas producto y categoria

--- developershop/settings.py
+++ developershop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "web",
+    "django_admin_tailwind",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -128,3 +131,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Producto
+
+admin.site.register(Categoria)
+# admin.site.register(Producto) <---- ESTA ES LA FORMA DE ANTES DE HACERLO
+
+
+@admin.register(Producto)  # <--------   ESTA ES LA NUEVA FORMA
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "precio",
+        "categoria",
+        "fecha_registro",
+    )
+    list_editable = (
+        "precio",
+        "categoria",
+    )
