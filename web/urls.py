2b7f514dcdd81a72b1b04df6d18bce4e8c8846ea
Fabrizio Bautista <130857074+fabrixbau@users.noreply.github.com>
2024-03-20 11:48:57 -0600

    se agrego la logica de carrito, add, delete,clear,save y se agregaron urls, views y templates

--- web/urls.py
+++ web/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
     ),
     path("productosPorNombre", views.productosPorNombre, name="productosPorNombre"),
     path("producto/<int:producto_id>", views.productoDetalle, name="producto"),
+    path("carrito", views.carrito, name="carrito"),
+    path(
+        "agregarCarrito/<int:producto_id>", views.agregarCarrito, name="agregarCarrito"
+    ),
+    path(
+        "eliminarProductoCarrito/<int:producto_id>",
+        views.eliminarProductoCarrito,
+        name="eliminarProductoCarrito",
+    ),
+    path("limpiarCarrito", views.limpiarCarrito, name='limpiarCarrito'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Categoria, Producto
+from .carrito import Cart
 
 # Create your views here.
 """ VISTAS PARA EL CATALOGO DE PRODUCTOS """
@@ -46,3 +47,41 @@ def productoDetalle(request, producto_id):
     context = {"producto": objProducto}
 
     return render(request, "producto.html", context)
+
+
+""" VISTAS PARA EL CARRITO DE COMPRAAS """
+
+
+def carrito(request):
+    return render(request, "carrito.html")
+
+
+def agregarCarrito(request, producto_id):
+    if request.method == "POST":
+        cantidad = int(request.POST["cantidad"])
+    else:
+        cantidad = 1
+
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto, cantidad)
+
+    if request.method == "GET":
+        return redirect("/")
+
+    return render(request, "carrito.html")
+
+
+def eliminarProductoCarrito(request, producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request, "carrito.html")
+
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request, "carrito.html")
