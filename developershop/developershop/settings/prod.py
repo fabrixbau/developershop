c324d5636808a4bee4f25ac2d1cacd2fdf877dd1
Fabrizio Bautista <bautistafabrizio@gmail.com>
2024-06-04 20:19:35 -0600

    Fix import statement for paypalrestsdk

--- developershop/developershop/settings/prod.py
+++ developershop/developershop/settings/prod.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from paypalrestsdk import Payment
+import paypalrestsdk
 from pathlib import Path
 from .base import *
 import dj_database_url
