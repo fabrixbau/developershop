c414db5cab84b33fd139476695b36610c203212d
Fabrizio Bautista <bautistafabrizio@gmail.com>
2024-06-07 17:47:45 -0600

    update keys .env

--- developershop/developershop/settings/prod.py
+++ developershop/developershop/settings/prod.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USR'),
-        'PASSWORD': config('DATABASE_PWD'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
         'PORT': config('DATABASE_PORT', default='3306'),
     }
