356b6e6c9b31aea8ef9b6d3302408a902b7fb9cd
Fabrizio Bautista <bautistafabrizio@gmail.com>
2024-06-06 18:46:17 -0600

    Update database configuration for production

--- developershop/developershop/settings/prod.py
+++ developershop/developershop/settings/prod.py
@@ -80,7 +80,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
             "django.contrib.auth.password_validation."
@@ -105,7 +105,7 @@ PASSWORD_VALIDATORS = [
             "NumericPasswordValidator"
         ),
     },
-]
+]"""  """
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
