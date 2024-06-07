9a0ad8a3b0359e601998f293b53f5962ad97de96
Fabrizio Bautista <bautistafabrizio@gmail.com>
2024-06-07 17:33:27 -0600

    update data database

--- developershop/developershop/settings/prod.py
+++ developershop/developershop/settings/prod.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = "developershop.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT', default='3306'),
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
