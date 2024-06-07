1cd205fbc31ec8abb2839e3b902cba27e7b8ca77
Fabrizio Bautista <bautistafabrizio@gmail.com>
2024-06-06 18:08:45 -0600

    Configure static files for Heroku

--- developershop/developershop/settings/prod.py
+++ developershop/developershop/settings/prod.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -136,3 +136,5 @@ PAYPAL_TEST = config('PAYPAL_TEST', default=True, cast=bool)
 PAYPAL_USER_EMAIL = config('PAYPAL_USER_EMAIL')
 
 ADMIN_USER_EMAIL = config('ADMIN_USER_EMAIL')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
